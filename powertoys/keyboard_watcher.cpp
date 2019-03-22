f1fa3ed2e9391e4aa68086fc921a034e5a03d5c9
Bartosz Sosnowski <bartosz@janeasystems.com>
2019-03-22 20:33:35 +0100

    Improve handling of window moving while popup is displayed.

--- powertoys/keyboard_watcher.cpp
+++ powertoys/keyboard_watcher.cpp
@@ -10,15 +10,15 @@
 namespace {
   using stdclock = std::chrono::system_clock;
 
-  HHOOK kbhook_handle = NULL;
-  std::mutex kbhook_mutex;
-  std::condition_variable kbhook_cv;
+  HHOOK hook_handle = NULL;
+  std::mutex held_delay_mutex, signal_delay_mutex;
+  std::condition_variable held_delay_cv, signal_delay_cv;
   stdclock::time_point winkey_press_timestamp;
   bool winkey_pressed = false;
   bool winkey_signaled = false;
   std::function<void()> on_held_cb, on_relese_cb;
 
-  LRESULT CALLBACK keyboard_hook(int nCode, WPARAM wParam, LPARAM lParam) {
+  LRESULT CALLBACK hook_proc(int nCode, WPARAM wParam, LPARAM lParam) {
     auto kb_hook = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
     if (nCode != HC_ACTION) {
       goto call_next_hook;
@@ -27,13 +27,13 @@ namespace {
       goto call_next_hook;
     }
     if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
-      std::unique_lock<std::mutex> lock(kbhook_mutex);
+      std::unique_lock<std::mutex> lock(held_delay_mutex);
       winkey_pressed = true;
       winkey_press_timestamp = stdclock::now();
       lock.unlock();
-      kbhook_cv.notify_one();
+      held_delay_cv.notify_one();
     } else {
-      std::unique_lock<std::mutex> lock(kbhook_mutex);
+      std::unique_lock<std::mutex> lock(held_delay_mutex);
       winkey_pressed = false;
       if (winkey_signaled) {
         winkey_signaled = false;
@@ -42,23 +42,18 @@ namespace {
     }
 call_next_hook:
     // If we are in "signalled" state and keypress arrives - call on_held_cb
-    // again, to update windows postion. 
+    // again, to update windows postion. Give it some time to move the window. 
     if (winkey_signaled) {
-      std::thread([] {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        if (winkey_signaled) {
-          on_held_cb();
-        }
-      }).detach();
+      signal_delay_cv.notify_one();      
     }
-    return CallNextHookEx(kbhook_handle, nCode, wParam, lParam);
+    return CallNextHookEx(hook_handle, nCode, wParam, lParam);
   }
 
-  void kbhook_delay_thread_proc(int ms) {
+  void held_delay_thread_proc(int ms) {
     auto delay = std::chrono::milliseconds(ms);
     while (true) {
-      std::unique_lock<std::mutex> lock(kbhook_mutex);
-      kbhook_cv.wait(lock, [] { return winkey_pressed && !winkey_signaled; });
+      std::unique_lock<std::mutex> lock(held_delay_mutex);
+      held_delay_cv.wait(lock, [] { return winkey_pressed && !winkey_signaled; });
       lock.unlock();
       auto wait_time = stdclock::now() - winkey_press_timestamp;
       while (wait_time <= delay) {
@@ -72,18 +67,30 @@ call_next_hook:
       }
     }
   }
+
+  void signal_delay_thread_proc() {
+    while (true) {
+      std::unique_lock<std::mutex> lock(signal_delay_mutex);
+      signal_delay_cv.wait(lock, [] { return winkey_pressed && winkey_signaled; });
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      if (winkey_signaled) {
+        on_held_cb();
+      }
+    }
+  }
 }
  
 void start_winkey_watcher(int ms_delay, std::function<void()> on_held, std::function<void()> on_released) {
-  std::lock_guard<std::mutex> lock(kbhook_mutex);
-  if (kbhook_handle == NULL) {
+  std::lock_guard<std::mutex> lock(held_delay_mutex);
+  if (hook_handle == NULL) {
     winkey_pressed = false;
     winkey_signaled = false;
-    kbhook_handle = SetWindowsHookEx(WH_KEYBOARD_LL, keyboard_hook, GetModuleHandle(NULL), NULL);
-    if (kbhook_handle == NULL) {
+    hook_handle = SetWindowsHookEx(WH_KEYBOARD_LL, hook_proc, GetModuleHandle(NULL), NULL);
+    if (hook_handle == NULL) {
       throw std::runtime_error("Cannot install keyboard listener");
     }
-    std::thread(kbhook_delay_thread_proc, ms_delay).detach();    
+    std::thread(held_delay_thread_proc, ms_delay).detach();
+    std::thread(signal_delay_thread_proc).detach();
   }
   on_held_cb = on_held;
   on_relese_cb = on_released;
