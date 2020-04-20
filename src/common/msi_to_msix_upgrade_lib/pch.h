8ccf9cf3fe2244a6d23b9279fd45a357d563ed57
Rafael Rivera <rafael@withinwindows.com>
2020-04-20 03:36:57 -0700

    Move from sdk-provided cppwinrt-1.x to cppwinrt-2.x nuget package

--- src/common/msi_to_msix_upgrade_lib/pch.h
+++ src/common/msi_to_msix_upgrade_lib/pch.h
@@ -5,6 +5,8 @@
 
 #pragma warning (disable: 5205)
 #include <winrt/base.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
 #pragma warning (default: 5205)
 #include <Windows.h>
 #include <MsiQuery.h>
