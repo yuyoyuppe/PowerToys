27e57d4ce76473a1370b3a7b035db7db29355531
Andrey Nekrasov <a.yuyoyuppe@gmail.com>
2023-01-11 15:28:18 +0100

    fix module init

--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -167,6 +167,7 @@ int runner(bool isProcessElevated, bool openSettings, std::string settingsWindow
             L"modules/FileLocksmith/PowerToys.FileLocksmithExt.dll",
             L"modules/MeasureTool/PowerToys.MeasureToolModuleInterface.dll",
             L"modules/Hosts/PowerToys.HostsModuleInterface.dll",
+            L"modules/MouseWithoutBorders/PowerToys.MouseWithoutBordersModuleInterface.dll",
         };
         const auto VCM_PATH = L"modules/VideoConference/PowerToys.VideoConferenceModule.dll";
         if (const auto mf = LoadLibraryA("mf.dll"))
